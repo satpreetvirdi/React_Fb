39192319e90075283d4079c0e25483f4367aa6f7
satpreet07 <satpreetvirdi007@gmail.com>
2021-04-22 23:12:32 +0530

    Facebook

---
+++ fb-clone/src/App.js
@@ -0,0 +1,35 @@
+
+import './App.css';
+import Feed from './Feed';
+import Header from './Header';
+import Login from './Login';
+import Sidebar from './Sidebar';
+import Widgets from './Widgets';
+import {useStateValue} from "./StateProvider";
+
+function App() {
+  const [{user}, dispatch] = useStateValue();
+  return (
+    <div className="App">
+     {!user ?(
+       <Login />
+     ) :(
+      <>
+      <Header />    
+      <div className="app__body">
+        <Sidebar />
+        <Feed />
+        <Widgets />
+        
+      </div>
+    
+      </> 
+     )
+
+     }
+         
+      </div>
+  );
+}
+
+export default App;
